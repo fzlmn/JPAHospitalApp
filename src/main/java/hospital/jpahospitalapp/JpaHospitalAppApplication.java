97fb8c4c6ce8dba9568b3b1b2e14d52b7266136f
Fatima Zahra <ait29lamine5@gmail.com>
2025-10-22 11:12:15 +0100

    project just created

---
+++ src/main/java/hospital/jpahospitalapp/JpaHospitalAppApplication.java
@@ -0,0 +1,13 @@
+package hospital.jpahospitalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHospitalAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaHospitalAppApplication.class, args);
+    }
+
+}
---
+++ src/main/java/hospital/jpahospitalapp/entities/Patient.java
@@ -0,0 +1,4 @@
+package hospital.jpahospitalapp.entities;
+
+public class Patient {
+}
